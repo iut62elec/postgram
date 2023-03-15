aa182289ac2cec104be44ea026cb5dd45e7145b4
jpedram <jpedram@amazon.com>
2023-03-14 20:13:09 -0400

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,110 @@
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
+import {
+  HashRouter,
+  Switch,
+  Route
+} from "react-router-dom";
 import React, {useState, useEffect, useRef} from 'react';
-import {Amplify, API, Auth, graphqlOperation} from 'aws-amplify'
+import {Amplify, API, Auth, graphqlOperation, Storage} from 'aws-amplify'
 import { Flex, View, Heading, MapView, Text, Image,  withAuthenticator, AmplifyS3Image } from '@aws-amplify/ui-react';
 import config from './aws-exports'
 import { listPosts } from './graphql/queries'
+import CreatePost from './CreatePost';
+import { Link } from 'react-router-dom';
+import { css } from '@emotion/css';
+
+
 Amplify.configure(config)
 
 
 
 
 
-function App({ signOut, user }) {
-  const [posts, setPosts] = useState ([]);
+function Router({ signOut, user }) {
+  const [posts_ped, setPosts_ped] = useState ([]);
+  //const file = e.target.files[0];
+
+  /* create a couple of pieces of initial state */
+  const [showOverlay, updateOverlayVisibility] = useState(false);
+  const [posts, updatePosts] = useState([]);
+
+
   useEffect(()=>{
     API.graphql(graphqlOperation(listPosts))
-    .then(response => setPosts(response.data.listPosts.items))
+    .then(response => setPosts_ped(response.data.listPosts.items))
     .then(error => console.log(error))
+    
   },[]);
-
+  console.log(posts)
   useEffect(() => {
     Auth.currentAuthenticatedUser()
     .then(response => console.log(response.attributes.email))
     .then(error => console.log(error))
 
   },[])
+  
 
+   /* fetch posts when component loads */
+   useEffect(() => {
+    fetchPosts();
+    }, []);
+
+    
+
+async function fetchPosts() {
+
+  /* query the API, ask for 100 items */
+  let postData = await API.graphql({ query: listPosts, variables: { limit: 100 }});
+  let postsArray = postData.data.listPosts.items;
+
+  /* map over the image keys in the posts array, get signed image URLs for each image */
+  postsArray = await Promise.all(postsArray.map(async post => {
+    const imageKey = await Storage.get(post.image);
+    post.image = imageKey;
+    return post;
+  }));
+
+  /* update the posts array in the local state */
+  setPostState(postsArray);
+}
+
+async function setPostState(postsArray) {
+  updatePosts(postsArray);
+}
+const imageStyle = css`
+  width: 100%;
+  max-width: 400px;
+`
   
   return (
     <div>
       <h1>Hello World</h1>
       {
-        posts.map(post => (
-          <div key={post.id}>
-            <h3>{post.name}</h3>
-            <p>{post.location}</p>
+        posts.map(post_ped => (
+          <div key={post_ped.id}>
+            <h3>{post_ped.name}</h3>
+            <p>{post_ped.location}</p>
+            <img alt="post" className={imageStyle}  src={post_ped.image} />
           </div>
         ))
       }
       <button onClick={signOut}>Sign Out</button>
+
+      <h1>Hello World</h1>
+      <button onClick={() => updateOverlayVisibility(true)}>New Post</button>
+
+
+      {showOverlay && (
+      <CreatePost
+            updateOverlayVisibility={updateOverlayVisibility}
+            updatePosts={setPostState}
+            posts={posts}
+      />
+      )}
+      
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default withAuthenticator(Router);
